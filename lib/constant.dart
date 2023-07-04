a4f8b2131e88c9b14eb476cb2c0920556b426c1f
JeremyLoh <jjeremyloh1234@gmail.com>
2023-07-04 15:44:25 +0800

    Update primary color

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color.fromRGBO(148, 0, 0, 1);
-const kPrimary100Color = Color.fromRGBO(255, 235, 235, 1);
+const kPrimaryColor = Color.fromRGBO(4 , 99, 128, 1);
+const kPrimary100Color = Color.fromRGBO(225 , 244, 250, 1);
+const kCompleted = Color.fromRGBO(6 , 95, 70, 1);
+const k100Completed = Color.fromRGBO(209 , 250, 229, 1);
 const kSecondaryColor = Color.fromRGBO(4, 8, 116, 1);
 const kLightBlue = Color.fromRGBO(244, 247, 255, 1);
 const kDisabledText = Color.fromARGB(255, 152, 152, 152);
