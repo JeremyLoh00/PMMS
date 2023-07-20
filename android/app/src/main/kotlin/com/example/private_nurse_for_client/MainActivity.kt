1d08d8c7aec3a4430fc30f7cd3cc29dde2140867
Izzat Rizal <86939634+ejjat0909@users.noreply.github.com>
2023-07-20 18:06:03 +0800

    install stripe payment

--- android/app/src/main/kotlin/com/example/private_nurse_for_client/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/private_nurse_for_client/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.private_nurse_for_client
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
